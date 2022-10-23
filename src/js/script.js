a6ddee2f05b05822a3f76ab01a46cd114bd88be9
Andrey Voronov <my.web.vaa@gmail.com>
2022-10-23 19:05:56 +0300

    v.2.0 add color picker

--- src/js/script.js
+++ src/js/script.js
@@ -2,24 +2,28 @@ window.addEventListener('DOMContentLoaded', () => {
     const paintBox = document.querySelector('.paintbox'),
           btn = document.querySelector('.btn__wrap'),
           btnOneStep = document.querySelector('.one-step'),
-          btnLongStep = document.querySelector('.long-step');
+          btnLongStep = document.querySelector('.long-step'),
+          getColor = document.querySelector('.swatchy-output');
 
-    //создаем поле
+    getColor.style.backgroundColor = '#03A9F4';
+
+          //создаем поле
     for (let i = 1; i <= 400; i++) {
         paintBox.innerHTML += '<div class="pixel"></div>';
     }
-
+    
     const arrPixels = document.querySelectorAll('.pixel');
     
     let clickCount = [], //флаг для отслеживания - будет ли ли кисть рисовать в режиме long step или нет
         stepChoice = true, //переменная для хранения статуса oneStep
         erase = false, //переменная для хранения режима стерательной резинки
         activeErase = false, //переменная для хранения статуса стирания - включен или нет
+        backgroundColor = 'rgb(239, 248, 247)',
 
         //создаем функцию добавления пикселя для обработчика события mouseover, когда в режиме long step пиксели закрашиваются при движении мышкой (вывел в отдельную функцию, чтоб можно было удалить обработчик событий)
         setPixel = function(e) {
             if (e.target.classList.contains('pixel')) {
-                !erase ? e.target.classList.add('pixelCircle') : e.target.classList.remove('pixelCircle'); //проверяет режим, если режим стирания - до очищает пиксель, иначе закрашивает
+                !erase ? e.target.style.backgroundColor = getColor.style.backgroundColor : e.target.style.backgroundColor = backgroundColor; //проверяет режим, если режим стирания - до очищает пиксель, иначе закрашивает
                 clickCount[0] = 1;
             }
         },
@@ -77,13 +81,13 @@ window.addEventListener('DOMContentLoaded', () => {
                     paintBox.classList.toggle('paintbox__longstep'); //деактивируем бордер у поля игры, чтоб видеть, что longstep режим неактивен и мы можем передвигаться по полю не закрашивая клетки
                     clickCount = [];
                 } else {
-                    !erase ? event.target.classList.add('pixelCircle') : event.target.classList.remove('pixelCircle'); //проверяет режим, если режим стирания - то очищает пиксель, иначе закрашивает
+                    !erase ? event.target.style.backgroundColor = getColor.style.backgroundColor : event.target.style.backgroundColor = backgroundColor; //проверяет режим, если режим стирания - то очищает пиксель, иначе закрашивает
                     paintBox.addEventListener('mouseover', setPixel);
                     paintBox.classList.toggle('paintbox__longstep'); //активируем бордер у поля игры, чтоб видеть, что longstep режим активен и движение закрашивает клетки
                     paintBox.addEventListener('mouseout', cursorСheck);
                 } 
             } else {
-                !erase ? event.target.classList.add('pixelCircle') : event.target.classList.remove('pixelCircle'); //проверяет режим, если режим стирания - до очищает пиксель, иначе закрашивает
+                !erase ? event.target.style.backgroundColor = getColor.style.backgroundColor : event.target.style.backgroundColor = backgroundColor; //проверяет режим, если режим стирания - до очищает пиксель, иначе закрашивает
             }
         }
     });
